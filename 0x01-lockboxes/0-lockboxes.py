4a81247a89b2906627a4aaaf79aeb73768ef8bcc
Ibrahim <ibrahimsani4.es@gmail.com>
2024-07-04 10:45:00 -0700

    update 0-lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,11 +4,30 @@
 
 def canUnlockAll(boxes):
     """Write a method that determines if all the boxes can be opened"""
-    n = 0
+    if not boxes:
+        return False
 
-    for key in boxes:
-        if not isinstance(key, list):
-            return True
-        else:
-            return False
-    return
+    # Create a list to keep track of visited boxes
+    visited = [False] * len(boxes)
+    visited[0] = True
+
+    # Create a stack to store boxes to be visited
+    stack = [0]
+
+    # Perform depth-first search to visit all boxes
+    while stack:
+        # Pop the top box from the stack
+        box = stack.pop()
+
+        # Iterate through the keys in the current box
+        for key in boxes[box]:
+            # Check if the key is valid and the corresponding box
+            # has not been visited
+            if 0 <= key < len(boxes) and not visited[key]:
+                # Mark the box as visited
+                visited[key] = True
+                # Add the box to the stack for further exploration
+                stack.append(key)
+
+    # Check if all boxes have been visited
+    return all(visited)
