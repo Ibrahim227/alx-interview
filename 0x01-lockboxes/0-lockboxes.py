de152e0d60e26493ac1628b3d2fa1be745731e37
Ibrahim <ibrahimsani4.es@gmail.com>
2024-07-04 10:12:50 -0700

    pdate 0-lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,4 +3,15 @@
 
 
 def canUnlockAll(boxes):
-        
+    """Write a method that determines if all the boxes can be opened"""
+    boxes = [[]]
+    n = 0
+
+    for key in boxes:
+        if isinstance(key, list):
+            if key < 0:
+                return
+        else:
+            n = canUnlockAll(key)
+            return True
+        return False
