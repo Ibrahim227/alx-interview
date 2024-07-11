45bf3bb00c87e515f4c2c43d0e67ac2ad3e018a2
Ibrahim <mamansasni.ibrahim01@gmail.com>
2024-07-11 16:33:12 -0700

    update 0-mineoperations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Import required module/lib"""
+
+
+def minOperations(n) -> int:
+    """
+        write a method that calculates the fewest number
+        of operations needed to result in exactly n H characters in the file
+    Returns: integer
+    """
+    if not n:
+        return 0
+    else:
+        print('ok')
