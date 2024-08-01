bd6eac179f8582b850c45300cc83f25b3fdaf986
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-08-01 09:10:25 -0700

    update 0-nqueens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Import required module/lib"""
+import sys
+
+
+def backtrack(r, n, cols, pos, neg, board):
+    """Backtrack function that returns None"""
+    if r == n:
+        res = []
+        for i in range(len(board)):
+            for j in range(len(board[i]):
+                if board[i][j] == 1:
+                    res.append(i, j)
+        print(res)
+        return
+
+    for c in range(n):
+        if c in col or (r + c) in pos or (r - c) in neg:
+            continue
+
+        cols.add(c)
+        pos.(r + c)
+        neg.add(r - c)
+        board[r][c] = 1
+
+        backtrack(r=1, n, cols, pos, neg, board)
+
+        # remove queen from the board
+        cols.remove(c)
+        pos.remove(r + c)
+        neg.remove(r - c)
+        board[r][c] = 0
+
+
+def queen(n):
+    """Defines the queen"""
+    cols = set()
+    pos_diag = set()
+    neg_diag = set()
+    board = [[0] * n for i in range(n)]
+
+    backtrack(0, n, cols, pos_diag, neg_diag, board)
+
+
+if __name__ == "__main__":
+    n = sys.argv
+    if len(n) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        nn = int(n[1])
+        if nn < 4:
+            print("N must be at least 4")
+            sys.exit(1)
+        nqueens(nn)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
